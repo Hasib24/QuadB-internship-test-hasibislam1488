f0a3cbda63473bffa889f456b686ff500a73997d
Hasib24 <hasibislam1488@gmail.com>
2023-06-06 18:42:55 +0600

    add nav design from react-bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header>
-            header
+            <Navbar bg="light" expand="lg">
+              <Container>
+                <Navbar.Brand href="/">QuadB</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="me-auto">
+                    <NavLink className='nav-link'>Home</NavLink>
+                    <NavLink className='nav-link'>About</NavLink>
+                  </Nav>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
         </header>
     );
 };
