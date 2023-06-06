28cc89808dd0bcb433aa287c6f8d7b4b71ccbf64
Hasib24 <hasibislam1488@gmail.com>
2023-06-06 18:25:41 +0600

    setup react router with some essential pages and components

---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer';
+
+const HomeLayout = () => {
+    return (
+        <main>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </main>
+    );
+};
+
+export default HomeLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/Router'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <div>
-    Hello world!
-  </div>
+  <RouterProvider router={router}></RouterProvider>
 )
